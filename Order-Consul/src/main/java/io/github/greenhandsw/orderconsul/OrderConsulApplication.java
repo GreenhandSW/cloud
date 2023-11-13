fc50b57d9f91fdc89f53562f40245578500b893a
GreenhandSW <20255611+GreenhandSW@users.noreply.github.com>
2023-11-13 15:53:27 +0800

    删除无用的扫描注解

--- Order-Consul/src/main/java/io/github/greenhandsw/orderconsul/OrderConsulApplication.java
+++ Order-Consul/src/main/java/io/github/greenhandsw/orderconsul/OrderConsulApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EntityScan("io.github.greenhandsw.core.entity")
 public class OrderConsulApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderConsulApplication.class, args);
--- Order-Eureka/src/main/java/io/github/greenhandsw/ordereureka/OrderEurekaApplication.java
+++ Order-Eureka/src/main/java/io/github/greenhandsw/ordereureka/OrderEurekaApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EntityScan("io.github.greenhandsw.core.entity")
 public class OrderEurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderEurekaApplication.class, args);
