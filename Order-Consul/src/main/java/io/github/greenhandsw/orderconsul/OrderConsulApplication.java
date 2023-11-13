7f72f25793e83ae9194388fd868e6a18893fd35e
GreenhandSW <20255611+GreenhandSW@users.noreply.github.com>
2023-11-13 15:53:27 +0800

    删除无用的扫描注解

--- Order-Consul/src/main/java/io/github/greenhandsw/orderconsul/OrderConsulApplication.java
+++ Order-Consul/src/main/java/io/github/greenhandsw/orderconsul/OrderConsulApplication.java
@@ -2,12 +2,10 @@ package io.github.greenhandsw.orderconsul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EntityScan("io.github.greenhandsw.core.entity")
 public class OrderConsulApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderConsulApplication.class, args);
