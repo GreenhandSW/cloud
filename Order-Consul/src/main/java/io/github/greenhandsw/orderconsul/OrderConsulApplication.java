56751f9e71359f6ad1c419a256a0c18b2bd168c2
GreenhandSW <20255611+GreenhandSW@users.noreply.github.com>
2023-11-13 18:58:06 +0800

    Revert "暂存"
    This reverts commit 861d1ef148aa0c952a77b883b3436633d17f99f6.

--- Order-Consul/src/main/java/io/github/greenhandsw/orderconsul/OrderConsulApplication.java
+++ Order-Consul/src/main/java/io/github/greenhandsw/orderconsul/OrderConsulApplication.java
@@ -2,6 +2,7 @@ package io.github.greenhandsw.orderconsul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
