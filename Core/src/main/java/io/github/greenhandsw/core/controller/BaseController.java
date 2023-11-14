b37b358c465a074d6682de8786f374102361c02b
GreenhandSW <20255611+GreenhandSW@users.noreply.github.com>
2023-11-14 10:58:29 +0800

    统一BaseController中crud操作的日志格式，提供crud操作的默认日志方法

--- Core/src/main/java/io/github/greenhandsw/core/controller/BaseController.java
+++ Core/src/main/java/io/github/greenhandsw/core/controller/BaseController.java
@@ -6,6 +6,7 @@ import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,8 @@ import java.util.List;
 
 @Slf4j
 public class BaseController<T, S extends BaseService<T, ID, R>, ID extends Serializable, R extends JpaRepository<T, ID>> {
-
+    @Resource
+    public Registration registration;
     @Resource
     protected S s;
 
@@ -25,14 +27,14 @@ public class BaseController<T, S extends BaseService<T, ID, R>, ID extends Seria
     @PostMapping("/add")
     public Object add(@RequestBody T entity){
         T result=s.save(entity);
-        log.info(">>>插入结果："+result);
+        doLog("插入", entity, result);
         return new CommonResult<T>(200, "插入成功", entity);
     }
 
     @PostMapping("/get")
     public Object get(@RequestBody ID id){
         T result=s.find(id);
-        log.info(">>>查询结果："+result);
+        doLog("查询", id, result);
         if(result==null){
             return new CommonResult<T>(200, "查询失败，不存在id=%s的记录".formatted(id));
         }
@@ -42,14 +44,14 @@ public class BaseController<T, S extends BaseService<T, ID, R>, ID extends Seria
     @PostMapping("/delete")
     public Object delete(@RequestBody ID id){
         s.delete(id);
-        log.info(">>>删除ID："+id);
+        doLog("删除", id, null);
         return new CommonResult<ID>(200, "删除成功", id);
     }
 
     @PostMapping("/update")
     public Object update(@RequestBody T entity){
         T result = s.update(entity);
-        log.info(">>>更新结果："+result);
+        doLog("更新", entity, result);
         return new CommonResult<T>(200, "更新成功", entity);
     }
 
@@ -68,5 +70,9 @@ public class BaseController<T, S extends BaseService<T, ID, R>, ID extends Seria
         }
 
         return discoveryClient;
+    }
+
+    public void doLog(String operation, Object input, Object output){
+        log.info("{}:{}>>>{}信息：{}>>>结果：{}", registration.getHost(), registration.getInstanceId(), operation, input, output);
     }
 }
